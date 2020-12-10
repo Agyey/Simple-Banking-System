00ac543834c926ef689b51de8b7b348d2f3ebc91
Agyey Arya <agyey27@yahoo.co.in>
2020-12-10 18:48:52 +0530

    Database Storage

--- banking.py
+++ banking.py
@@ -1,12 +1,15 @@
 from random import randint
 from datetime import datetime
+import sqlite3
+
 
 class Account:
 
-    def __init__(self, opening_balance: float=0):
+    def __init__(self, con, opening_balance: float=0):
         self.card_number: str = self.generate_number()
         self.card_pin: str = self.generate_pin()
         self.balance: float = opening_balance
+        self.con = con
         self.logged_in: bool = False
         self.services = [
             {'Balance': 'show_balance'},
@@ -24,7 +27,7 @@ class Account:
 
     @staticmethod
     def generate_pin() -> str:
-        return str(randint(0, 10*4-1)).rjust(4, '0')
+        return str((hash(datetime.now()) + hash(randint(0, 10*5-1))) % 10**4).rjust(4, '0')
 
     @staticmethod
     def generate_checksum(number):
@@ -35,9 +38,14 @@ class Account:
         for i in range(len(nums)):
             if nums[i] > 9:
                 nums[i] -= 9
-        return str(10 - sum(nums) % 10)
+        return str((10 - sum(nums) % 10) % 10)
 
     def create_account(self):
+        cur = self.con.cursor()
+        sql = f"INSERT INTO card (number, pin) " \
+              f"VALUES ('{self.card_number}', '{self.card_pin}');"
+        cur.execute(sql)
+        self.con.commit()
         print("\nYour card has been created")
         print("Your card number:")
         print(self.card_number)
@@ -46,10 +54,12 @@ class Account:
         return False
 
     def login_account(self, credentials: dict) -> bool:
-        if self.card_number == credentials['card_number'] and self.card_pin == credentials['card_pin']:
-            print('\nYou have successfully logged in!\n')
-            self.logged_in = True
-            return self.ask_user()
+        print("\nYou have successfully logged in!\n")
+        self.card_number = credentials['card_number']
+        self.card_pin = credentials['card_pin']
+        self.balance = credentials['balance']
+        self.logged_in = True
+        return self.ask_user()
 
     def logout_account(self):
         self.logged_in = False
@@ -81,13 +91,13 @@ class Account:
 
 class Bank:
 
-    def __init__(self):
+    def __init__(self, con):
         self.open = True
         self.services = [
             {'Create an account': 'create_account'},
             {'Log into an account': 'login_account'}
         ]
-        self.accounts: list = []
+        self.con = con
 
     def is_open(self):
         return self.open
@@ -99,9 +109,8 @@ class Bank:
         self.open = False
 
     def create_account(self):
-        account = Account()
+        account = Account(self.con)
         account.create_account()
-        self.accounts.append(account)
         return True
 
     def login_account(self):
@@ -111,11 +120,20 @@ class Bank:
         credentials['card_number'] = input()
         print("Enter your PIN:")
         credentials['card_pin'] = input()
-        for account in self.accounts:
+        cur = self.con.cursor()
+        sql = f"SELECT balance FROM card " \
+              f"WHERE number = '{credentials['card_number']}' " \
+              f"AND pin = '{credentials['card_pin']}';"
+        cur.execute(sql)
+        result = cur.fetchall()
+        if result:
+            credentials['balance'] = result[0][0]
+            account = Account(self.con)
             account_response = account.login_account(credentials)
             if account_response:
                 return account_response
-        print('\nWrong card number or PIN!\n')
+        else:
+            print('\nWrong card number or PIN!\n')
         return False
 
     def ask_user(self):
@@ -137,7 +155,19 @@ class Bank:
                 print(f"Enter Values between 0-{len(self.services)}")
         except NameError:
             print(f"Enter Values between 0-{len(self.services)}")
-bank = Bank()
-bank.open_bank()
-while bank.is_open():
-    bank.ask_user()
+
+
+with sqlite3.connect('card.s3db') as con:
+    con = sqlite3.connect('card.s3db')
+    cur = con.cursor()
+    sql = "CREATE TABLE IF NOT EXISTS card(" \
+          "id INTEGER PRIMARY KEY AUTOINCREMENT," \
+          "number TEXT," \
+          "pin TEXT," \
+          "balance INTEGER DEFAULT 0);"
+    cur.execute(sql)
+    con.commit()
+    bank = Bank(con)
+    bank.open_bank()
+    while bank.is_open():
+        bank.ask_user()
